debf04552c00621b89cbcce5e83ecef9458cccb4
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2025-04-15 11:39:39 -0500

    removed unused files and modified App.css content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+
+  return (
+    <>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
